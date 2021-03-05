4c78c21b7bbc0c55e470f7776b0b0c7d12336269
kukaro <justkukaro@naver.com>
2021-03-06 06:41:17 +0900

    feat : getter and setter

--- 80p-getter-setter.dart
+++ 80p-getter-setter.dart
@@ -1,10 +1,25 @@
-class Person{
+class Person {
   String name;
   int _age;
+  String _item;
 
   int get age => _age;
+  String get item => _item;
+
+  set item(_item) {
+    this.item = _item;
+  }
+
+  @override
+  String toString() {
+    return 'Person{name: $name, _age: $_age, _item: $_item}';
+  }
 }
 
-void main(){
-
+void main() {
+  var person = new Person();
+  person.name = 'kukaro';
+  person._age = 10;
+  person._item = 'knife';
+  print(person);
 }
