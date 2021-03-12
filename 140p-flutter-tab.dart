b477e1fa271449ce0b58dc6ba7ccc6005eb7e2eb
kukaro <justkukaro@naver.com>
2021-03-12 11:30:12 +0900

    feat : flutter widget of a tab

---
+++ 140p-flutter-tab.dart
@@ -0,0 +1,62 @@
+// Copyright (c) 2019, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  final String title;
+
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Tab'),
+          bottom: TabBar(
+            tabs: <Widget>[
+              Tab(icon: Icon(Icons.tag_faces)),
+              Tab(text: '메뉴2'),
+              Tab(icon: Icon(Icons.info), text: '메뉴3'),
+            ],
+          ),
+        ),
+        body: TabBarView(children: <Widget>[
+          Container(
+            color: Colors.yellow,
+          ),
+          Container(
+            color: Colors.orange,
+          ),
+          Container(
+            color: Colors.red,
+          ),
+        ]),
+      ),
+    );
+  }
+}
